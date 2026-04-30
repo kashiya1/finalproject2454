794226ec3d9465dab4165bfafd4d77a850cec613
kashiya1 <kashiyakelley60@gmail.com>
2026-04-30 13:05:10 -0400

    go

---
+++ config.inc.php
@@ -0,0 +1,9 @@
+
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'storesdb');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBCONNSTRING', 'mysql:host=' . DBHOST . ';dbname=' . DBNAME .
+';charset=utf8mb4');
+
